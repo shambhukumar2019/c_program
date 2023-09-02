b818156a5de56420445bad891ed3b31653b1b9f8
shambhu-kumar <kshambhu67515@gmail.com>
2023-09-02 15:29:55 +0530

    print input number in HEX

--- basic/posnum.c
+++ basic/posnum.c
@@ -6,6 +6,7 @@ int main(void)
 	printf("*-------Check if a Number is Positive or Negative-------*\n");
 	printf("enter number: ");
 	scanf("%d",&user_num);
+	printf("your number: 0x%X\n",user_num);
 	if(user_num >= 0)
 		printf("given number is positive\n");
 	else
