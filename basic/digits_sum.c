53ccb3211e22213f2b3d901b6b9c8b4526d77d6d
shambhu-kumar <kshambhu67515@gmail.com>
2023-09-05 10:33:57 +0530

    add comments

--- basic/digits_sum.c
+++ basic/digits_sum.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+// calculate sum of digits in a given integer value
 
 int main(void)
 {
@@ -9,12 +10,14 @@ int main(void)
 	printf("enter a value: ");
 	scanf("%d",&value);
 	
+	//find sum if digits
 	while(value != 0)
 	{
-		remainder = value % 10;
-		value /= 10;
-		sum += remainder;
+		remainder = value % 10;	//get each digits one by one as 'remiander'
+		value /= 10;			//remove the fetched digit from 'value'
+		sum += remainder;		//add each digits and store in 'sum'
 	}
 	printf("result = %d\n",sum);
+
 	return 0;
 }
