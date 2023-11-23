b7b5f94b6f5302d691751302b22d37852594cb6c
shambhu-kumar <kshambhu67515@gmail.com>
2023-11-23 08:32:41 +0530

    comment

--- basic/pos_num.c
+++ basic/pos_num.c
@@ -3,14 +3,15 @@
 int main(void)
 {
 	int user_num;
+	//comment
 	printf("*-------Check if a Number is Positive or Negative-------*\n");
-	printf("enter number: ");
+	printf("enter number: ");//comment
 	scanf("%d",&user_num);
 	printf("your number: 0x%X\n",user_num);
 	if(user_num >= 0)
 		printf("given number is positive\n");
 	else
 		printf("given number is negative\n");
-	
+
 	return 0;
 }
